4582ea004c9bed485c27fe4b75e96861ccb9209b
GouravSaini125 <gdinsan777@gmail.com>
2019-08-23 22:00:07 +0530

    2 Commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn sainishadi.wsgi --log-file -
--- sainishadi/settings.py
+++ sainishadi/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -128,3 +129,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
 
 AUTH_USER_MODEL = 'sainishadiApp.User'
+
+# import django_heroku
+# django_heroku.settings(locals())
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
